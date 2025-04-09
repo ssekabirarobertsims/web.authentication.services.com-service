1af1fa9d521193c007ed445f9ba5df892b206215
ssekabira robert sims <ssekabirarobert037@gmail.com>
2025-04-09 16:55:10 +0300

    update index.html file page and 404 settings for api *********09/04/2025 16:57**************

--- controller/middleware/error/404.middleware.controller.js
+++ controller/middleware/error/404.middleware.controller.js
@@ -4,15 +4,7 @@ module.exports = async function (request, response) {
   response.contentType = ["plain/text", "text/html", "Application/json"];
   response.statusCode = Number.parseInt(404);
 
-  if (request.accepts("plain/text")) {
-    response.status(Number.parseInt(404)).send("404 Not Found!");
-  } else if (request.accepts("text/html")) {
-    response
+  response
       .status(Number.parseInt(404))
       .sendFile(require("node:path").join(__dirname, "../../../view/404.html"));
-  } else {
-    response.status(Number.parseInt(404)).jsonp({
-      "ERROR 404": "Not Found!",
-    });
-  }
 };
