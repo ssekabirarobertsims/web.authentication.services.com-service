e523a711108f19a3809d9646620946cdb1a54c54
ssekabira robert sims <ssekabirarobert037@gmail.com>
2025-04-09 16:58:48 +0300

    updated settings for jsonwebtoken/jwt  *********09/04/2025 17:5**************

--- controller/middleware/jwt/jwt.middleware.controller.js
+++ controller/middleware/jwt/jwt.middleware.controller.js
@@ -3,6 +3,8 @@ debugger;
 const jwt = require("jsonwebtoken");
 const model_connection = require("../../../model/connection/model.connection");
 require("dotenv").config();
+const { v4: uuid } = require("uuid");
+const { format } = require("date-fns");
 
 module.exports = async function (request, response, next) {
   try {
@@ -17,11 +19,17 @@ module.exports = async function (request, response, next) {
       response.status(402).jsonp({
         error: "Invalid token provided!",
         message: "Unauthorized",
+        status_code: (response.statusCode = Number(parseInt(402))),
+        request_id: uuid(),
+        date: format(new Date(), "yyyy-MM-dd\tHH:mm:ss"),
       });
     } else if (!token || !AuthorizationHeaders) {
       response.status(400).jsonp({
         error: "No token provided!",
         message: "Bad request",
+        status_code: (response.statusCode = Number(parseInt(400))),
+        request_id: uuid(),
+        date: format(new Date(), "yyyy-MM-dd\tHH:mm:ss"),
       });
     } else {
       next();
@@ -30,9 +38,12 @@ module.exports = async function (request, response, next) {
     response.status(400).jsonp({
       error: error.message,
       information: {
-        data: "token might be missing or invalid!",
+        data: "Token might be missing or invalid!",
       },
       message: "Bad request",
+      status_code: (response.statusCode = Number(parseInt(400))),
+      request_id: uuid(),
+      date: format(new Date(), "yyyy-MM-dd\tHH:mm:ss"),
     });
     console.log(error);
   }
